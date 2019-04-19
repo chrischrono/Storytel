29aeceb96106579b14150a94162c9052253fc654
Christian <chris_chrono@yahoo.com>
2019-04-19 17:01:34 +0700

    Add Networking Layer

---
+++ Storytel/Service/Networking/EndPoint/StorytelAPIEndPoint.swift
@@ -0,0 +1,60 @@
+//
+//  StorytelAPIEndPoint.swift
+//  NetworkLayer
+//
+// source: https://medium.com/flawless-app-stories/writing-network-layer-in-swift-protocol-oriented-approach-4fa40ef1f908
+
+import Foundation
+
+
+public enum StorytelAPI {
+    case searchBooks(query: String, page: String)
+}
+
+extension StorytelAPI: EndPointType {
+    
+    /** API base urls. */
+    var environmentBaseURL : String {
+        switch StorytelAPINetworkManager.environment {
+        case .production:
+            return "https://api.storytel.net/"
+        default:
+            return "https://api.storytel.net/"
+        }
+    }
+    
+    var baseURL: URL {
+        guard let url = URL(string: environmentBaseURL) else { fatalError("baseURL could not be configured.")}
+        return url
+    }
+    /** API path for specific request. */
+    var path: String {
+        switch self {
+        case .searchBooks:
+            return "search"
+        }
+    }
+    
+    var httpMethod: HTTPMethod {
+        return .get
+    }
+    
+    /** generate task based on requested GitHubAPI. */
+    var task: HTTPTask {
+        switch self {
+        //https://api.storytel.net/search?query=harry&page=10
+        case .searchBooks(let query, let page):
+            return .requestParameters(bodyParameters: nil,
+                                      bodyEncoding: .urlEncoding ,
+                                      urlParameters: ["query": query,
+                                                      "page": page])
+        }
+    }
+    
+    var headers: HTTPHeaders? {
+        return nil
+    }
+}
+
+
+
---
+++ Storytel/Service/Networking/Manager/NetworkManager.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkManager.swift
+//  NetworkLayer
+//
+//
+
+import Foundation
+
+
+protocol StorytelAPINetworkProtocol {
+    init(environment: NetworkEnvironment)
+    /**
+     Search Books's data, that contain query's string,  based on StorytelAPI
+     - Parameter query: search keyword for book list
+     - Parameter page: requested page of book list
+     - Parameter completion: block to handle the fetch results
+     */
+    func searchBooks(query: String, page: String, completion: @escaping (_ queryBooksResponse: QueryBooksResponse?,_ error: String?)->())
+    
+}
+
+class StorytelAPINetworkManager: StorytelAPINetworkProtocol {
+    static var environment : NetworkEnvironment = .production
+    let router = Router<StorytelAPI>()
+    
+    required init(environment: NetworkEnvironment) {
+        StorytelAPINetworkManager.environment = environment
+    }
+    
+    /**
+     Search Books's data, that contain query's string,  based on StorytelAPI
+     - Parameter query: search keyword for book list
+     - Parameter page: requested page of book list
+     - Parameter completion: block to handle the fetch results
+     */
+    func searchBooks(query: String, page: String, completion: @escaping (_ queryBooksResponse: QueryBooksResponse?,_ error: String?)->()) {
+        router.request(.searchBooks(query: query, page: page) ) { (data, response, error) in
+            guard let data = data, error == nil else {
+                completion(nil, error)
+                return
+            }
+            do {
+                /*let jsonData = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                print(jsonData)*/
+                let queryBooksResponse = try JSONDecoder().decode(QueryBooksResponse.self, from: data)
+                completion(queryBooksResponse, nil)
+            }catch {
+                print(error)
+                completion(nil, NetworkResponse.unableToDecode.rawValue)
+            }
+        }
+    }
+}
